22e1d74a472b2f91926d78b81137dbf6a19baf40
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2024-01-25 08:33:12 +0000

    Add dbt module in main

--- tf/main.tf
+++ tf/main.tf
@@ -9,3 +9,9 @@ module "composer" {
     project_ID = var.project_ID
     resource_location = var.resource_location
 }
+
+module "dbt" {
+    source     = "./modules/dbt"
+    project_ID = var.project_ID
+    resource_location = var.resource_location
+}
