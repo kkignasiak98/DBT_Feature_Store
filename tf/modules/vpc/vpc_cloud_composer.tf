9d5954224cd363a37878ef81c41dab17ef799a88
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2024-01-31 07:52:45 +0000

    add vpc module

---
+++ tf/modules/vpc/vpc_cloud_composer.tf
@@ -0,0 +1,12 @@
+resource "google_compute_network" "composer_network" {
+  name                    = "composer-network"
+  auto_create_subnetworks = false
+  project = var.project_ID
+}
+
+resource "google_compute_subnetwork" "composer_subnetwork" {
+  name          = "composer-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = var.resource_location
+  network       = google_compute_network.composer_network.id
+}
