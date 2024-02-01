4b0d997d45019538879a0cbc4ed80114308f5f61
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2024-02-01 09:03:59 +0000

    make variable about terragrunt version

--- install_terragrunt.bash
+++ install_terragrunt.bash
@@ -1,13 +1,14 @@
 # Define constants
 TERRAGRUNT_VERSION="v0.54.22"
+TERRAGRUNT_INSTALL_FILE="terragrunt_linux_amd64"
 # Download terragrunt 
-wget https://github.com/gruntwork-io/terragrunt/releases/download/$TERRAGRUNT_VERSION/terragrunt_linux_amd64
+wget https://github.com/gruntwork-io/terragrunt/releases/download/$TERRAGRUNT_VERSION/$TERRAGRUNT_INSTALL_FILE
 
 # Make the download executible
-chmod +x terragrunt_linux_amd64
+chmod +x $TERRAGRUNT_INSTALL_FILE
 
 # Move the binary to a directory in your PATH
-sudo mv terragrunt_linux_amd64 /usr/local/bin/terragrunt
+sudo mv $TERRAGRUNT_INSTALL_FILE /usr/local/bin/terragrunt
 
 # Verify the installation
 terragrunt --version
